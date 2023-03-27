95a4421f0a365e5a2f9c7e9ee6ae1a70c387cf22
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-27 10:24:23 +0900

    Initial commit

---
+++ src/main/java/com/example/n_plus_1/NPlus1Application.java
@@ -0,0 +1,13 @@
+package com.example.n_plus_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NPlus1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NPlus1Application.class, args);
+    }
+
+}
